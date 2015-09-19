913dd446a7291d7d8887e951146c736cd994337b
latter-jacob <latter.jacob@gmail.com>
2015-09-19 16:32:54 -0400

    made the user page.....waiting for backend

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -5,6 +5,9 @@ validates_presence_of :name, :price, :date, :time
   CATEGORIES = %w[italian american seafood sushi chinese thai vietnamese desserts bakery japanese hamburgers cajun pizza]
   validates :category, inclusion: { in: CATEGORIES }
 
+  PRICES = %w[$ $$ $$$ $$$$ $$$$$]
+  validates :prices, inclusion: { in: PRICES }
+
   has_many :groups
   has_many :users,
     through: :groups
