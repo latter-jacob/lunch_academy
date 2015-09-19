28ee48a43c7a446cfba69cd059008a13105658d0
Drake McCabe <drakemccabe@gmail.com>
2015-09-19 16:40:12 +0000

    added index method to lunches controller

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,6 +1,7 @@
 class LunchesController < ApplicationController
 
   def index
+    @lunch = Lunch.where("date_time > current_timestamp - (20 * interval '1 minute')").order(:date_time)
   end
 
   def new
