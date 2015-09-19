257291c0a6dcc51ba4e1da1b134a7d4b6809fac1
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-09-19 14:30:14 -0400

    added shruggy and other categories

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,8 +2,8 @@ class Lunch < ActiveRecord::Base
 
   validates_presence_of :name, :price, :date_time
 
-  CATEGORIES = %w[italian american seafood sushi chinese thai vietnamese desserts bakery japanese hamburgers cajun pizza]
-  validates :category, inclusion: { in: CATEGORIES }
+  CATEGORIES = %w[¯\\_(ツ)_/¯ italian american seafood sushi chinese thai vietnamese desserts bakery japanese hamburgers cajun pizza other]
+  validates :category, inclusion: { in: CATEGORIES, allow_blank: true }
 
   has_many :groups
   has_many :users,
